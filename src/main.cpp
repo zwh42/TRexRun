05f8fe56940a3a54feadd651fcb5ffbc18b5da70
zhao wenhao <zhaowenhao@outlook.com>
2019-09-30 22:46:05 +0800

    SDL init test

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    SDL_Window * window = nullptr;
+
+    SDL_Surface * window_surface = nullptr;
+    SDL_Surface * image_surface = nullptr;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+
+    window_surface = SDL_GetWindowSurface(window);
+    image_surface = IMG_Load("../res/trex.png");
+
+    SDL_BlitSurface(image_surface, NULL, window_surface, NULL);
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Event e;
+bool quit = false;
+while (!quit){
+    while (SDL_PollEvent(&e)){
+        if (e.type == SDL_QUIT){
+            quit = true;
+        }
+        if (e.type == SDL_KEYDOWN){
+            quit = true;
+        }
+        if (e.type == SDL_MOUSEBUTTONDOWN){
+            quit = true;
+        }
+    }
+}
+
+    SDL_DestroyWindow(window);
+    SDL_FreeSurface(image_surface);
+    SDL_Quit();
+}
